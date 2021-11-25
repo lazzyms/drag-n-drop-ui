6bd4f173f5b888c99e39ef1a897fc132c38dce72
maulikcm <maulik.sompura@codemonk.in>
2021-11-25 13:00:29 +0530

    no scroll for canvas

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <Sidebar />
 
       <div className='md:pl-64'>
-        <div className='m-2 bg-gray-100 h-screen flex flex-col'>
-          <main className='flex-1 '>
+        <div className='m-2 flex flex-col screen'>
+          <main className='flex-1'>
             {/* Main canvas component */}
             <Canvas />
           </main>
